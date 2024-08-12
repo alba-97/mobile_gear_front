076f74cb202e6ba7416ac5a865278dc138403ba8
alba-97 <alvarezbasilio97@gmail.com>
2024-08-11 23:05:30 -0300

    Change config

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,42 @@
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import istanbul from 'vite-plugin-istanbul';
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
+import istanbul from "vite-plugin-istanbul";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-ignore
-import tsconfig from './tsconfig.json';
+import tsconfig from "./tsconfig.json";
 
 // automatically map path aliases
 const rawAlias = tsconfig.compilerOptions.paths;
 const alias = {};
 
 for (const x in rawAlias) {
-	alias[x.replace('/*', '')] = rawAlias[x].map((p) =>
-		path.resolve(__dirname, p.replace('/*', '')),
-	);
+  alias[x.replace("/*", "")] = rawAlias[x].map((p) =>
+    path.resolve(__dirname, p.replace("/*", ""))
+  );
 }
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
-	process.env = loadEnv(mode, process.cwd(), '');
-	return defineConfig({
-		resolve: {
-			alias,
-		},
-		plugins: [
-			react(),
-			istanbul({
-				cypress: true,
-				requireEnv: false,
-			}),
-		],
+  process.env = loadEnv(mode, process.cwd(), "");
+  return defineConfig({
+    resolve: {
+      alias,
+    },
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      istanbul({
+        cypress: true,
+        requireEnv: false,
+      }),
+    ],
 
-		server: {
-			host: true,
-			port: process.env.PORT ? +process.env.PORT : 5173,
-		},
-	});
+    server: {
+      host: true,
+      port: process.env.PORT ? +process.env.PORT : 5173,
+    },
+  });
 };
