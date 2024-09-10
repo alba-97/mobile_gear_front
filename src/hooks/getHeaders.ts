545037bb951fbbf1a9b9bd4090bebeb90d66b54c
alba-97 <alvarezbasilio97@gmail.com>
2024-09-10 02:06:39 -0300

    Send empty jwt instead of null

--- src/hooks/getHeaders.ts
+++ src/hooks/getHeaders.ts
@@ -1,4 +1,4 @@
 export default function getHeaders() {
-  const jwt = localStorage.getItem("jwt");
+  const jwt = localStorage.getItem("jwt") ?? "";
   return { headers: { Authorization: `Bearer ${jwt}` } };
 }
